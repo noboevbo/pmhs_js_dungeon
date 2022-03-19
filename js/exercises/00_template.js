d98d8d6782695fa005f75caf41c056463a33bac0
Dennis Burgermeister <dennis@dennisnotes.com>
2022-03-19 11:37:10 +0100

    (f) fixed template

--- js/exercises/00_template.js
+++ js/exercises/00_template.js
@@ -1,17 +1,29 @@
-import { validate } from "../exercise/validation_helper.js";import { Exercise } from '../exercise/exercise_base.js';
+import {
+  TODO
+} from '../exercise/validation_helper.js';
+import { Exercise } from '../exercise/exercise_base.js';
 
 let exerciseID = "TODO: insert id";
 
+let instructions = `
+<ol>
+    <li>TODO</li>
+</ol>`
+
 let validationFuncs = [
-  // TODO: add checks and return {result: xy, errorMessage: xy}, see check_helper for examples
+  // TODO: add exercises and return {result: xy, errorMessage: xy}, see validation_helper for examples
   function() { return TODO }
 ]
 
 let tips = [
-  TODO
+  {
+    level: 0,
+    title: "",
+    content: ``,
+    weblinks: [""],
+    contentIsHTML: true
+}
 ]
 
-window.onload = function() { 
-  window.parent.initializeTips(exerciseID, tips);
-  validate(exerciseID, validationFuncs); 
-};
+let exerciseBase = new Exercise(exerciseID, instructions, tips, validationFuncs);
+window.onload = exerciseBase.init();
